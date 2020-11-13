aa1fd0200e60f8b2e5293f5e50aa798585a03c09
Alex Samari <alexsamarim@gmail.com>
2020-11-13 18:19:40 -0500

    update application routing to include basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieComponent } from './components/movie/movie.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'movies',
+    component: MovieComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'movies',
+  },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
