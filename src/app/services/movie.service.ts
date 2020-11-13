ecb55d7165beb94b4b5fd5bf2931da0cedb2e0d6
Alex Samari <alexsamarim@gmail.com>
2020-11-13 18:20:23 -0500

    create movie service

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { shareReplay, map, catchError } from 'rxjs/operators';
+import { Movie } from '../models/movie.model';
+import { throwError } from 'rxjs/internal/observable/throwError';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+  private httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: 'Bearer Wookie2019',
+    }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  getMovies = (): Observable<Movie[]> => {
+    return this.http
+      .get<Movie[]>(`${environment.apiURI}/movies`, this.httpOptions)
+      .pipe(
+        map((data: any) => data.movies),
+        shareReplay(),
+        catchError(() => throwError('User not found'))
+      );
+  };
+}
