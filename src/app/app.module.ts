8beeeb915a41f3be80fe2f3258633f3496be18a1
Alex Samari <alexsamarim@gmail.com>
2020-11-13 16:12:31 -0500

    add material module to include material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieComponent } from './components/movie/movie.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MovieComponent
-  ],
+  declarations: [AppComponent, MovieComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
