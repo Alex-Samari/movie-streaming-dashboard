6ab832cb8c2ffd95835af66c4247a604ae27b0ff
Alex Samari <alexsamarim@gmail.com>
2020-11-13 17:18:33 -0500

    design header

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [],
+  exports: [
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
 })
 export class MaterialModule {}
