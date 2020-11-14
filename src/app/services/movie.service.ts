219f085edebf9201a25c8d1c4ccfb268bb346558
Alex Samari <alexsamarim@gmail.com>
2020-11-13 21:37:16 -0500

    add getMoviesByQuery api

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
@@ -15,6 +15,7 @@ export class MovieService {
     headers: new HttpHeaders({
       Authorization: 'Bearer Wookie2019',
     }),
+    params: null,
   };
 
   constructor(private http: HttpClient) {}
@@ -25,6 +26,17 @@ export class MovieService {
       .pipe(
         map((data: any) => data.movies),
         shareReplay(),
+        catchError(() => throwError('User not found'))
+      );
+  };
+
+  getMoviesByQuery = (queryParameter: string): Observable<Movie[]> => {
+    this.httpOptions.params = new HttpParams().set('q', queryParameter);
+
+    return this.http
+      .get<Movie[]>(`${environment.apiURI}/movies`, this.httpOptions)
+      .pipe(
+        map((data: any) => data.movies),
         catchError(() => throwError('User not found'))
       );
   };
