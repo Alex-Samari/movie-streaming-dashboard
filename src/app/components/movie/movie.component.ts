321f875938072f9f2e52bb6186359bd0fae07a8c
Alex Samari <alexsamarim@gmail.com>
2020-11-13 19:51:03 -0500

    complete movies page

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie.model';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.scss']
+  styleUrls: ['./movie.component.scss'],
 })
 export class MovieComponent implements OnInit {
+  genres: Movie['genres'] = [];
+  movies: Movie[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private movieService: MovieService) {
+    this.movieService.getMovies().subscribe((movies) => {
+      this.movies = movies;
+      // Get movie genres from the list of movies
+      for (const movie of movies) {
+        for (const genre of movie.genres) {
+          if (
+            !this.genres.find(
+              (savedGenre) => savedGenre === genre.toLocaleLowerCase()
+            )
+          ) {
+            this.genres.push(genre.toLocaleLowerCase());
+          }
+        }
+      }
+      this.genres.sort();
+    });
   }
 
+  ngOnInit(): void {}
+
+  isGenreInMovie = (movie: Movie, genre: string) => {
+    if (
+      movie.genres.find(
+        (item) => item.toLocaleLowerCase() === genre.toLocaleLowerCase()
+      )
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 }
