d1b348acffdcd0c51df61bba20752adfa3e02e03
Alex Samari <alexsamarim@gmail.com>
2020-11-13 21:40:05 -0500

    complete movie detail component

--- src/app/components/movie-detail/movie-detail.component.ts
+++ src/app/components/movie-detail/movie-detail.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from 'src/app/models/movie.model';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.scss']
+  styleUrls: ['./movie-detail.component.scss'],
 })
 export class MovieDetailComponent implements OnInit {
+  private movieId: Movie['id'];
+  selectedMovie: Movie = null;
+  rating = 0;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private movieService: MovieService
+  ) {
+    this.activatedRoute.params.subscribe((params: any) => {
+      this.movieId = params.movieId;
+    });
 
-  ngOnInit(): void {
+    /**
+     * following code is inefficient and can be removed with an API that gets the movie by Id
+     * Currently the qurey API does NOT accept movie IDs as a parameter
+     */
+    this.movieService.getMovies().subscribe((movies) => {
+      this.selectedMovie = movies.find((movie) => movie.id === this.movieId);
+      this.rating = Math.floor(this.selectedMovie.imdb_rating / 2);
+    });
   }
 
+  ngOnInit(): void {}
+
+  getYearFromDateString = (dateString: string) => {
+    const year = new Date(dateString).getFullYear();
+    return year;
+  };
+
+  getCastString = (cast: Movie['cast']): string => {
+    const castString = cast.join(', ');
+    return castString;
+  };
 }
